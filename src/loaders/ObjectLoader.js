12802a1fbf956ba8c71ec0219913e05ebfba07c0
Satya Ortiz-Gagné <sortizgagne@cogniens.com>
2016-11-17 16:51:18 -0500

    add partial support for TextGeometry in ObjectLoader
    data.data.font needs to be an instance of THREE.Font so this can't come from a pure JSON as would expect the ObjectLoader

--- src/loaders/ObjectLoader.js
+++ src/loaders/ObjectLoader.js
@@ -268,6 +268,15 @@ THREE.ObjectLoader.prototype = {
 
 						break;
 
+					case 'TextGeometry':
+
+						geometry = new THREE.TextGeometry(
+							data.text,
+							data.data
+						);
+
+						break;
+
 					case 'BufferGeometry':
 
 						geometry = bufferGeometryLoader.parse( data );
